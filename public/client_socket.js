6d539f09642ec141ce12ba6b6fdce1c531cb3665
Michael <behm.39@osu.edu>
2019-11-02 19:42:59 -0400

    Updated other protocol

--- public/client_socket.js
+++ public/client_socket.js
@@ -1,6 +1,6 @@
 
 const urlParams = new URLSearchParams(window.location.search);
-let url = `http://${window.location.hostname}:${window.location.port}`
+let url = `${window.location.protocol}://${window.location.hostname}:${window.location.port}`
 var socket = io(url, {query: `num=${urlParams.get("num")}`});
 
 socket.on('announcement', (data) => {
