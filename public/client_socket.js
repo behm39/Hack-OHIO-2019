8dfa55c469964b69f52134b55dc23e3534c753f9
Michael <behm.39@osu.edu>
2019-11-02 19:45:13 -0400

    logging for heroku

--- public/client_socket.js
+++ public/client_socket.js
@@ -1,6 +1,7 @@
 
 const urlParams = new URLSearchParams(window.location.search);
 let url = `${window.location.protocol}://${window.location.hostname}:${window.location.port}`
+console.log(`URL: ${url}`);
 var socket = io(url, {query: `num=${urlParams.get("num")}`});
 
 socket.on('announcement', (data) => {
