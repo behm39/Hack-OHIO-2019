8e4769bd6cb8dc6a8b9a9a3d4553c76931212963
Michael <behm.39@osu.edu>
2019-11-02 19:22:17 -0400

    Added file upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const sockets = require('./socket');
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
 
 var server = app.listen(8000, () => {
     console.log('App listening on port 8000!');
@@ -17,6 +20,16 @@ app.get('/room', (req, res) => {
     } else {
         return res.redirect("https://google.com/teapot");
     }
+});
+
+app.post('/host-upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    console.log(req.files);
+    let upload = req.files.myfile;
+    
+    return res.send("The file '" + upload.name + "' with data '" + upload.data + "' has been uploaded...");
 });
 
 app.get('/host', (req, res) => {
