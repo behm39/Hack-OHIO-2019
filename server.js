dcd90ba0072daaace982e293279ffd53db30858f
Michael <behm.39@osu.edu>
2019-11-02 11:55:04 -0400

    stub in test html file

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
+app.get('/socket-demo', (req, res) => {
+    res.sendFile(__dirname + 'socket.html');
+});
+
 app.listen(8000, () => {
     console.log('App listening on port 8000!');
 });
