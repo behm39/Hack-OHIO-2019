a019e9b2437105572a132eb6de1546a89f7e3919
Patrick Hubbell <hubbell.64@osu.edu>
2019-11-02 12:42:16 -0400

    create simple file upload feature

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const app = express();
+
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile('upload.html', {
+        root: path.join(__dirname, './')
+    })
+});
+
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let upload = req.files.testFile;
+
+    res.send("The file '" + upload.name + "' with data '" + upload.data + "' has been uploaded...");
 });
 
 app.listen(8000, () => {
